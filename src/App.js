e0af05caddcc4a951b7d1dba27e326c2ff3560a3
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-07-26 08:43:15 -0300

    Se instaló dependencia antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <h1>Starter Template with Antd</h1>;
+  return( 
+
+    <h1>Starter Template with Antd Mobile</h1>
+  
+  )
 };
 
 export default App;
---
+++ src/components/ui/layout/MainLayout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainLayout(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MainLayout;
