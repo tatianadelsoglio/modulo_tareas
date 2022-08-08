a7645ff3afdc64004c93ff13bb630280d0ff53bb
tatianadelsoglio <tatianadelsoglio96@gmail.com>
2022-08-08 10:15:26 -0300

    Se modifico archivo ListaTarea.js, se creo un array de objeto de las tareas y se mejoro el componente de lista utilizando un map, además se simplico componente swipeAction

--- src/Components/ui/listaTareas/ListaTarea.js
+++ src/Components/ui/listaTareas/ListaTarea.js
@@ -1,13 +1,12 @@
 import { Dialog, Ellipsis, List, Modal, SwipeAction } from "antd-mobile";
-import React, { useContext, useRef} from "react";
+import React, { useContext, useRef } from "react";
 import { useHistory } from "react-router-dom";
-import { EditSOutline, CheckOutline } from 'antd-mobile-icons';
+import { EditSOutline, CheckOutline } from "antd-mobile-icons";
 import "./ListaTarea.css";
 import { GlobalContext } from "../../context/GlobalContext";
 
 const ListaTarea = () => {
-
-  const {fecha} = useContext(GlobalContext);
+  const { fecha } = useContext(GlobalContext);
 
   let history = useHistory();
 
@@ -17,104 +16,119 @@ const ListaTarea = () => {
     history.push("/detalletarea");
   };
 
-
   const handleModalCerrar = () => {
-      Modal.alert({
-        header: (
-          <CheckOutline
-            style={{
-              fontSize: 64,
-              color: 'var(--adm-color-primary)',
-            }}
-          />
-        ),
-        title: 'Tarea Cerrada Correctamente',
-        confirmText:"Cerrar",
-      });
+    Modal.alert({
+      header: (
+        <CheckOutline
+          style={{
+            fontSize: 64,
+            color: "var(--adm-color-primary)",
+          }}
+        />
+      ),
+      title: "Tarea Cerrada Correctamente",
+      confirmText: "Cerrar",
+    });
   };
 
+  const rightActions = [
+    {
+      key: "editar",
+      text: <EditSOutline />,
+      color: "#2bc4e3",
+      onClick: async () => {
+        await Dialog.confirm({
+          content: "¿Editar Tarea?",
+          cancelText: "Cancelar",
+          confirmText: "Aceptar",
+          onConfirm: handleModalDetalleTarea,
+        });
+        ref.current?.close();
+      },
+    },
+    {
+      key: "cerrar",
+      text: <CheckOutline />,
+      color: "primary",
+      onClick: async () => {
+        await Dialog.confirm({
+          content: "¿Cerrar Tarea?",
+          cancelText: "Cancelar",
+          confirmText: "Aceptar",
+          onConfirm: handleModalCerrar,
+        });
+        ref.current?.close();
+      },
+    },
+  ];
+
+  const ItemListaTarea = [
+    {
+      description: "Adrian Sabo",
+      //extra:{fecha},
+      content: "Llamar a Adrian, conversar sobre nuevos insumos",
+    },
+    {
+      description: "Horacio Mercol",
+      //extra:{fecha},
+      content: "Visitar Campo Oeste",
+    },
+    {
+      description: "Jorge Mayorga",
+      content: "Llamar a Jorge para Venta de Herbicidas",
+    },
+    {
+      description: "Aida Campos",
+      content: "Venta Trigo",
+    },
+    {
+      description: "Adrian Sabo",
+      content: "Venta de Maíz",
+    },
+    {
+      description: "Florencia Caverzasi",
+      content: "Venta de Soja",
+    },
+    {
+      description: "Adrian Sabo",
+      content: "Venta de Maíz para temporada 2223",
+    },
+    {
+      description: "Adrian Sabo",
+      //extra:{fecha},
+      content: "Llamar a Adrian, conversar sobre nuevos insumos",
+    },
+    {
+      description: "Horacio Mercol",
+      //extra:{fecha},
+      content: "Visitar Campo Oeste",
+    },
+    {
+      description: "Jorge Mayorga",
+      content: "Llamar a Jorge para Venta de Herbicidas",
+    },
+    {
+      description: "Aida Campos",
+      content: "Venta Trigo",
+    },
+  ];
 
   return (
     <>
       <div className="div_tareas">
         <List>
-          <SwipeAction
-            ref={ref}
-            closeOnAction={false}
-            closeOnTouchOutside={false}
-            rightActions={[
-              {
-                key: "editar",
-                text:<EditSOutline />,
-                color: "#2bc4e3",
-                onClick: async () => {
-                  await Dialog.confirm({
-                    content: "¿Editar Tarea?",
-                    cancelText: "Cancelar",
-                    confirmText: "Aceptar",
-                    onConfirm: handleModalDetalleTarea,
-                  });
-                  ref.current?.close();
-                },
-              },
-              {
-                key: "cerrar",
-                text:<CheckOutline />,
-                color: "primary",
-                onClick: async () => {
-                  await Dialog.confirm({
-                    content: "¿Cerrar Tarea?",
-                    cancelText: "Cancelar",
-                    confirmText: "Aceptar",
-                    onConfirm: handleModalCerrar,
-                  });
-                  ref.current?.close();
-                },
-              },
-            ]}
-          >
-            <List.Item description="Adrian Sabo" extra={fecha}>
-              <Ellipsis
-                direction="end"
-                content="Llamar a Adrian, conversar sobre nuevos insumos"
-              />
-            </List.Item>
-          </SwipeAction>
-
-          <List.Item description="Horacio Mercol" extra="29-07-2022">
-            <Ellipsis direction="end" content="Visitar Campo Oeste" />
-          </List.Item>
-
-          <List.Item description="Adrian Sabo" extra="29-07-2022">
-            <Ellipsis direction="end" content="Venta de Soja" />
-          </List.Item>
-          <List.Item description="Adrian Sabo" extra="30-07-2022">
-            <Ellipsis
-              direction="end"
-              content="Venta de Maíz para temporada 2022"
-            />
-          </List.Item>
-          <List.Item description="Adrian Sabo" extra="30-07-2022">
-            <Ellipsis
-              direction="end"
-              content="Llamar a Adrian, conversar sobre nuevos insumos"
-            />
-          </List.Item>
-          <List.Item description="Horacio Mercol" extra="31-07-2022">
-            <Ellipsis direction="end" content="Visitar Campo Oeste" />
-          </List.Item>
-          <List.Item description="Adrian Sabo" extra="31-07-2022">
-            <Ellipsis direction="end" content="Venta de Soja" />
-          </List.Item>
-          <List.Item description="Jorge Mayorga" extra="01-08-2022">
-            <Ellipsis
-              direction="end"
-              content="Llamar a Jorge para Venta de Herbicidas"
-            />
-          </List.Item>
-          <List.Item description="Aida Campos" extra="01-08-2022">
-            <Ellipsis direction="end" content="Venta de Maíz" />
-          </List.Item>
+          {ItemListaTarea.map((ItemListaTarea) => (
+            <SwipeAction
+              ref={ref}
+              closeOnAction={false}
+              closeOnTouchOutside={false}
+              rightActions={rightActions}
+            >
+              <List.Item description={ItemListaTarea.description} extra={fecha}>
+                <Ellipsis direction="end" content={ItemListaTarea.content} />
+              </List.Item>
+            </SwipeAction>
+          ))}
         </List>
       </div>
     </>
